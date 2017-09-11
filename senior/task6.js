784393613474054f6e1180a17a48fdccea382c20
DeeJay <1018805743@qq.com>
2017-09-11 23:54:16 +0800

    publish/subscribe pattern

---
+++ senior/task6.js
@@ -0,0 +1,32 @@
+
+var Event = (function () {
+    var events = {};
+    function on (evt,callback) {
+        events[evt] = events[evt] || [];
+        events[evt].push(callback);
+    }
+    function fire(evt,args) {
+        if (!events[evt]) {
+            return;
+        }
+        events[evt][0](args);
+    }
+    function off(evt) {
+        delete events[evt];
+    }
+
+
+    return {
+        on: on,
+        fire: fire,
+        off: off,
+    }
+})();
+
+
+
+Event.on('change', function(val){
+    console.log('change...  now val is ' + val);
+});
+Event.fire('change', 'deejay');
+Event.off('changer');
