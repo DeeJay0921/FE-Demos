34ff1cf578e7a5819a1fe6c6bad7b548ff983395
unknown <yangbin>
2017-10-31 15:09:07 +0800

    add simulate-select

--- senior/task4-requireJS/src/js/main.js
+++
@@ -1,8 +0,0 @@
-requirejs.config({
-    baseUrl: 'src/js',
-    path: {
-        // jquery: 'lib/jquery-3.2.1.min',
-    }
-})
-
-requirejs(['app/index']);
--- vueDemos/v-model.js
+++
@@ -1,8 +0,0 @@
-var vm = new Vue({
-    el: '.app',
-    data() {
-        return {
-            something: ''
-        }
-    }
-})
